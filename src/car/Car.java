42ddc02e9a5249520565d5d53e4d1388ae654aeb
Olexandra <olexandra.dmytrenko@gmail.com>
2025-09-14 16:34:06 +0200

    Delete redundant folders from the repo

--- src/car/Car.java
+++ src/car/Car.java
@@ -21,7 +21,7 @@ public abstract class Car implements TransportMovement {
      * @param direction
      * @return
      */
-    @Override
+    @Overridek
     public boolean drive(int speed, GeographicalCoordinates direction) {
         return drive(speed, direction.transformToOneDimensional());
     }
